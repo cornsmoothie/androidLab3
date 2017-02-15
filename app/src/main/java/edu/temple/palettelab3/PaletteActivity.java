a0f168ec5988a615c5976f1a883a93dff1b9c7c7
George Keenan <georgekeenan@gmail.com>
2017-02-14 22:28:03 -0500

    First Commit

---
+++ app/src/main/java/edu/temple/palettelab3/PaletteActivity.java
@@ -0,0 +1,69 @@
+package edu.temple.palettelab3;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.RelativeLayout;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.List;
+
+import static edu.temple.palettelab3.R.id.activity_palette;
+
+public class PaletteActivity extends AppCompatActivity {
+
+    Spinner colorSpinner;
+    String[] colors = {"WHITE" , "RED" , "BLUE" , "GREEN" , "GRAY" , "PURPLE"};
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_palette);
+
+        colorSpinner = (Spinner) findViewById(R.id.spinnerOfColor);
+
+        final RelativeLayout pallette_activity = (RelativeLayout) findViewById(R.id.activity_palette);
+
+        final ArrayAdapter<String> colorAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, colors);
+
+        colorSpinner.setAdapter(colorAdapter);
+
+        AdapterView.OnItemSelectedListener spinnerListener = new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(PaletteActivity.this, colorSpinner.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
+                pallette_activity.setBackgroundColor(Color.parseColor(colorSpinner.getSelectedItem().toString()));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        };
+
+
+        colorSpinner.setOnItemSelectedListener(spinnerListener);
+    }
+    private class customadapter extends ArrayAdapter<String> {
+
+        public customadapter(Context context , List<String> objects) {
+            super(context , android.R.layout.simple_list_item_1 , objects);
+        }
+
+        @Override
+        public View getView(int position , View convertView , ViewGroup parent) {
+            View v = super.getView(position , convertView , parent);
+            v.setBackgroundColor(Color.parseColor(colorSpinner.getSelectedItem().toString()));
+            return v;
+        }
+
+}
+}
+
---
+++ app/src/main/java/edu/temple/palettelab3/customadapter.java
@@ -0,0 +1,12 @@
+package edu.temple.palettelab3;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.List;
+
+
